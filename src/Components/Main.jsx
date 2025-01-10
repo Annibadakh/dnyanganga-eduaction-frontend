738cdaf801c4fc1f4985873e427135a194dbf974
maheshchaudhari26 <patilmanav150@gmail.com>
2025-01-11 00:36:16 +0530

    nabbar added

---
+++ src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import Home from './Home.jsx';
+import Navbar from './Navbar.jsx';
+const Main = () => {
+    return(
+        <>
+        <Navbar />
+        <Home />
+        </>
+    )
+};
+
+export default Main;
