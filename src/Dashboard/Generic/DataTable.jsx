f1f45d4b1d27ddc400b62b8a78ec75f58515a859
Aniket Badakh <aniketbadakh1212@gmail.com>
2026-05-31 12:50:52 +0530

    payment table row colour

--- src/Dashboard/Generic/DataTable.jsx
+++ src/Dashboard/Generic/DataTable.jsx
@@ -7,6 +7,7 @@ const DataTable = ({
   error = null,
   rowKey = "id",
   emptyMessage = "No data found",
+  getRowClassName = () => "",
 }) => {
   return (
     <div className="bg-white shadow-custom md:p-6 p-2">
@@ -16,7 +17,6 @@ const DataTable = ({
       {!loading && !error && data.length > 0 ? (
         <div className="overflow-auto">
           <table className="table-auto w-full border text-center border-customgray overflow-hidden shadow-lg text-sm">
-            
             {/* Header */}
             <thead className="bg-primary text-customwhite uppercase tracking-wider">
               <tr>
@@ -36,7 +36,10 @@ const DataTable = ({
               {data.map((row, rowIndex) => (
                 <tr
                   key={row[rowKey] || rowIndex}
-                  className="border-b border-gray-200 hover:bg-gray-100 transition"
+                  className={`
+                    border-b border-gray-200 hover:bg-gray-100 transition
+                    ${getRowClassName(row, rowIndex)}
+                  `}
                 >
                   {columns.map((col, colIndex) => (
                     <td
@@ -51,13 +54,10 @@ const DataTable = ({
                 </tr>
               ))}
             </tbody>
-
           </table>
         </div>
       ) : (
-        !loading && (
-          <p className="text-lg text-black">{emptyMessage}</p>
-        )
+        !loading && <p className="text-lg text-black">{emptyMessage}</p>
       )}
     </div>
   );
